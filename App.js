1006d2eee7b568545726d1696d578df91e23a7b1
LiljestromNadja <118609353+LiljestromNadja@users.noreply.github.com>
2023-10-09 17:24:23 +0300

    Update App.js, valmis, siivottu

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-//import * as React from 'react';
-//import { useState } from 'react';
 import React, { useState } from 'react';
 import { View, StyleSheet, Button, TextInput, Keyboard } from 'react-native';
 import * as Speech from 'expo-speech';
-//import SegmentedControl from '@react-native-segmented-control/segmented-control' //npx expo install @react-native-segmented-control/segmented-control
 
 
 export default function App() {
 
   const [input, setInput] = useState();
-
-
-
 
   const speak = () => {
     //const thingToSay = '1';
@@ -19,11 +13,10 @@ export default function App() {
     Keyboard.dismiss();
     setInput('');
   };
+  
 
   return (
     <View style={styles.container}>
-
-
       <TextInput onChangeText={input => setInput(input)} value={input} placeholder='Type here what you want to hear'/>
       <Button title="Press to hear text" onPress={speak} />
     </View>
